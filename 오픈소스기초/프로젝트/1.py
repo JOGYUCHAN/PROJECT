d27df1113e7a05f651b2a9c6da7a4261d5c7df6b
JOGYCHAN <rbcks2234@gmail.com>
2022-05-27 13:22:50 +0900

    테스트

--- 오픈소스기초/프로젝트/1.py
+++ 오픈소스기초/프로젝트/1.py
@@ -1,6 +1,7 @@
 import tkinter
+from urllib.request import AbstractDigestAuthHandler
 
 window=tkinter.Tk()
 window.title("schedule management")
 
-window.mainloop()
+window.mainloop()AbstractDigestAuthHandler
